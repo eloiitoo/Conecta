78a1d211d035f864a219f655528eb3e2c62289b7
Pablo <Pablo@TOSH>
2012-06-29 19:00:56 +0200

    conflicto prueba

--- src/jugador2/Alg_Minimax.java
+++ src/jugador2/Alg_Minimax.java
@@ -42,7 +42,7 @@ public class Alg_Minimax {
 		
 		System.out.println("Utilidad de la raiz:"+ini.utilidad);
 		System.out.println("LA NO Utilidad de la raiz:"+ini.utilidad);
-		
+		ogogkg
 		
 		while(iterador.hasNext()){
 			Nodo actual=iterador.next();
